6751823c25158b4b9c1159a49e06f04560b5d789
Nicholas Myers <nic338@gmail.com>
2023-07-06 15:28:33 -0400

    finished exercise 4 chapter 3

---
+++ Gifter/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,15 @@
+﻿using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        List<UserProfile> GetAll();
+        UserProfile GetById(int id);
+        void Add(UserProfile userProfile);
+        void Update(UserProfile userProfile);
+        void Delete(int id);
+        UserProfile GetUserWithPosts(int id);
+
+    }
+}
